03d73c3d9b8995523b58d6ddec6ed53feeeb2637
xepiqq <jaydencrowther@gmail.com>
2025-10-03 07:55:17 -0600

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import { DeployButton } from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
 import { Hero } from "@/components/hero";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 
